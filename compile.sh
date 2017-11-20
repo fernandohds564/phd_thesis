0ef8e24da936d8ebb33748c3af967b584448699e
Fernando <fernandohds564@gmail.com>
2017-11-19 22:31:20 -0200

    Create appendices and glossary.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env /bin/bash
 
-rm *.aux *.log *.pdf
+rm *.aux *.log *.pdf compile/*
 pdflatex --interaction=nonstopmode -output-directory=compile tese.tex
 pdflatex --interaction=nonstopmode -output-directory=compile tese.tex
+
+
+# pdflatex --interaction=nonstopmode -output-directory=compile tese.tex
+# bibtex compile/tese.aux
+# makeindex --interaction=nonstopmode -output-directory=compile tese.idx
+# makeindex --interaction=nonstopmode -output-directory=compile tese.nlo -s nomencl.ist -o tese.nls
+# makeglossaries --interaction=nonstopmode -output-directory=compile tese.aux
+# pdflatex --interaction=nonstopmode -output-directory=compile tese.tex
+# pdflatex --interaction=nonstopmode -output-directory=compile tese.tex
